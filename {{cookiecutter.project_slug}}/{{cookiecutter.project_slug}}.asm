4f0f9a118b0e5d6964a7752aa29bc55537eb6097
Lars Peyer <lars.peyer@liip.ch>
2017-10-24 17:56:09 +0200

    fixed cookiecutter variable

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.asm
@@ -0,0 +1,6 @@
+;  Executable name : {{cookiecutter.project_slug}}
+;  Version         : 1.0
+;  Created date    : {% now 'local', '%d.%m.%Y' %}
+;  Last update     : {% now 'local', '%d.%m.%Y' %}
+;  Author          : {{cookiecutter.author}}
+;  Description     : {{cookiecutter.short_dec}}
