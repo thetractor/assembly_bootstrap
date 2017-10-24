990caa1eb6179ab2e9a18ee5d87c86f679f97340
Lars Peyer <lars.peyer@liip.ch>
2017-10-24 17:06:11 +0200

    initial bootstrap setup

---
+++ {{project_name}}/{{project_name}}.asm
@@ -0,0 +1,6 @@
+;  Executable name : {{project_name}}
+;  Version         : 1.0
+;  Created date    : {% now 'local', '%d.%m.%Y' %}
+;  Last update     : {% now 'local', '%d.%m.%Y' %}
+;  Author          : {{author}}
+;  Description     : {{short_dec}}
